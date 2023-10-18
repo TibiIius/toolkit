2cd85af13e920b62fb856f86cfd994d6ec4b0a7c
TimFB <oss@timfb.dev>
2023-10-18 19:55:14 +0200

    Update CI and build more containers:
    - Build with Docker actions instead of Buildah
    - Also build Fedora alongside Arch

---
+++ common/symlinks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak && \
+  ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman && \
+  ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman-compose && \
+  ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker && \
+  ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree && \
+  ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/buildah
